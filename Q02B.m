aacb22131df36af306bd38a1500edfc89365f9c3
Lucas-Barcaro <90339076+Lucas-Barcaro@users.noreply.github.com>
2025-04-21 16:13:33 -0300

    Add files via upload
    Q02B e Q02C

---
+++ Q02B.m
@@ -0,0 +1,18 @@
+n = -50:50;
+x = sin(0.125 * pi * n);
+[y, m] = dnsample(x, n, 4);
+
+% Plot
+figure;
+subplot(2,1,1);
+stem(n, x, 'filled');
+title('x(n) = sin(0.125πn)');
+xlabel('n'); ylabel('x(n)');
+grid on;
+
+subplot(2,1,2);
+stem(m, y, 'filled');
+title('y(n) = x(n) dizimado por M = 4');
+xlabel('n'); ylabel('y(n)');
+grid on;
+
